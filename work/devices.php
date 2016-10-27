2c22f2b7cd77b5c6251b1f72a2d7c4b5f55804e3
luckidu33 <luc.romain@ynov.com>
2016-10-27 12:34:01 +0200

    del group on device

---
+++ work/devices.php
@@ -0,0 +1,28 @@
+<?php
+
+function getDevices(){
+	$rep = curl('GET','/device',null,$_SESSION['token']);
+	if($rep[0] == "401") return "Session Timeout, Please authenticate...";
+	else if($rep[0] == "404") return "Error: curl returned 404 error...";
+	else if($rep[0] != "200") return "Error: Server connection issues....";
+        else return json_decode($rep[1],true);
+}
+
+function getDevicesGroups(){
+        $rep = curl('GET','/deviceGroup',null,$_SESSION['token']);
+        if($rep[0] == "401") return "Session Timeout, Please authenticate...";
+        else if($rep[0] == "404") return "Error: curl returned 404 error...";
+        else if($rep[0] != "200") return "Error: Server connection issues....";
+        else return json_decode($rep[1],true);
+}
+
+function deleteDevice($id){
+	if($id != ""){
+		$rep = curl('DELETE',"/device/$id",null,$_SESSION['token']);
+		if($rep[0] == "401") return "Session Timeout, Please authenticate...";
+		else if($rep[0] == "404") return "Error: curl returned 404 error...";
+		else if(!strstr($rep[0],"20")) return "Error: Server connection issues....";
+		else return true;
+	}
+	else return "Error: no user id specified...";
+}
---
+++ work/groups.php
@@ -0,0 +1,44 @@
+<?php
+
+function getUsers(){
+        $rep = curl('GET','/user',null,$_SESSION['token']);
+        if($rep[0] == "401") return "Session Timeout, Please authenticate...";
+        else if($rep[0] == "404") return "Error: curl returned 404 error...";
+        else if($rep[0] != "200") return "Error: Server connection issues....";
+        else return json_decode($rep[1],true);
+}
+
+function getGroups(){
+        $rep = curl('GET','/userGroup',null,$_SESSION['token']);
+        if($rep[0] == "401") return "Session Timeout, Please authenticate...";
+        else if($rep[0] == "404") return "Error: curl returned 404 error...";
+        else if($rep[0] != "200") return "Error: Server connection issues....";
+        else return json_decode($rep[1],true);
+}
+
+
+function addGroup($name,$users){
+	if($name!=""){
+		$datas = array(
+			"name" => $name,
+			"members" => $users
+		);
+		$rep = curl('POST','/userGroup',$datas,$_SESSION['token']);
+		if($rep[0] == "401") return "Session Timeout, Please authenticate...";
+                else if($rep[0] == "404") return "Error: curl returned 404 error...";
+                else if($rep[0] != "201") return "Error: Server connection issues....";
+                else return true;
+	}
+	else return "Error: specify group name";
+}
+
+function deleteGroup($id){
+	if($id != ""){
+		$rep = curl('DELETE',"/usergroup/$id",null,$_SESSION['token']);
+		if($rep[0] == "401") return "Session Timeout, Please authenticate...";
+		else if($rep[0] == "404") return "Error: curl returned 404 error...";
+		else if(!strstr($rep[0],"20")) return "Error: Server connection issues....";
+		else return true;
+	}
+	else return "Error: no group id specified...";
+}
